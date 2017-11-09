3152d17a69b91b693cc2a67d623c3fbd99420564
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-09 13:57:39 -0500

    Add get methods

--- LibraryBounds.java
+++ LibraryBounds.java
@@ -5,6 +5,22 @@ public class LibraryBounds {
 
     private final static double MAX_LON = 84.30696;
     private final static double MIN_LON = 84.30664;
+    
+    public static double getMaxLat() {
+        return MAX_LAT;
+    }
+
+    public static double getMinLat() {
+        return MIN_LAT;
+    }
+
+    public static double getMaxLon() {
+        return MAX_LON;
+    }
+
+    public static double getMinLon() {
+        return MIN_LON;
+    }
 
     public static boolean checkBounds(double lat, double lon) {
         if (lat < MAX_LAT && lat > MIN_LAT && lon < MAX_LON && lon > MIN_LON) {
