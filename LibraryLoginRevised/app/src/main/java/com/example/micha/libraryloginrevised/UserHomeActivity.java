4b2f59bb8c23b7929d797c1f3a9598435c0993f3
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-12 11:13:12 -0500

    Declare variables for fields

--- LibraryLoginRevised/app/src/main/java/com/example/micha/libraryloginrevised/UserHomeActivity.java
+++ LibraryLoginRevised/app/src/main/java/com/example/micha/libraryloginrevised/UserHomeActivity.java
@@ -2,6 +2,8 @@ package com.example.micha.libraryloginrevised;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserHomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class UserHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        final EditText txtName = (EditText) findViewById(R.id.txtName);
+        final TextView lblWelcome = (TextView) findViewById(R.id.lblWelcome);
     }
 }
