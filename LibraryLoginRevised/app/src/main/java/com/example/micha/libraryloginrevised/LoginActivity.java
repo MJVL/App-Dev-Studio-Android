02ffd4c94978c664e8a4000a24bd3b6e42ff4442
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-12 11:06:44 -0500

    Declare variables for fields

--- LibraryLoginRevised/app/src/main/java/com/example/micha/libraryloginrevised/LoginActivity.java
+++ LibraryLoginRevised/app/src/main/java/com/example/micha/libraryloginrevised/LoginActivity.java
@@ -2,6 +2,10 @@ package com.example.micha.libraryloginrevised;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText txtUsername = (EditText) findViewById(R.id.txtUserName);
+        final EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final TextView lblRegister = (TextView) findViewById(R.id.lblRegister);
+
+        lblRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
--- LibraryLoginRevised/app/src/main/java/com/example/micha/libraryloginrevised/RegisterActivity.java
+++ LibraryLoginRevised/app/src/main/java/com/example/micha/libraryloginrevised/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.micha.libraryloginrevised;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText txtName = (EditText) findViewById(R.id.txtName);
+        final EditText txtUsername = (EditText) findViewById(R.id.txtUserName);
+        final EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+        final Button btnRegister = (Button) findViewById(R.id.btnRegister);
     }
 }
