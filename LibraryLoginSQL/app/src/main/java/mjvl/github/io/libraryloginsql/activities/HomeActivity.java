ab842c03d32f06e5c5794cb7e50170ba8e6577dd
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-13 14:00:10 -0500

    Add basic checkBounds functionality

--- LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/HomeActivity.java
+++ LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/HomeActivity.java
@@ -5,13 +5,17 @@ package mjvl.github.io.libraryloginsql.activities;
  */
 
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import mjvl.github.io.libraryloginsql.R;
+import mjvl.github.io.libraryloginsql.helper.LibraryBounds;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -28,15 +32,23 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         lblStatus = (TextView) findViewById(R.id.lblStatus);
         btnCheck = (Button) findViewById(R.id.btnCheck);
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
-        lblWelcome.setText("Welcome " + nameFromIntent);
-
+        lblWelcome.setText("Logged in as " + nameFromIntent);
         btnCheck.setOnClickListener(this);
+        
     }
 
     @Override
     public void onClick(View v){
         switch (v.getId()){
-
+            case R.id.btnCheck:
+                double lat = 0, lon = 0;
+                if (LibraryBounds.checkBounds(lat,lon)) {
+                    Toast.makeText(getApplicationContext(), "In Bounds", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Out of Bounds", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
 }
