6ec270c3bb4161abc8b90e2578cd2ce0e26b23b1
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-13 12:16:08 -0500

    Modify HomeActivity to implement View.OnClickListener

--- LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/HomeActivity.java
+++ LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/HomeActivity.java
@@ -4,13 +4,15 @@ package mjvl.github.io.libraryloginsql.activities;
  * Created by micha on 11/12/2017.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import mjvl.github.io.libraryloginsql.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView lblWelcome;
 
@@ -21,5 +23,12 @@ public class HomeActivity extends AppCompatActivity {
         lblWelcome = (TextView) findViewById(R.id.lblWelcome);
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
         lblWelcome.setText("Welcome " + nameFromIntent);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+
+        }
     }
 }
