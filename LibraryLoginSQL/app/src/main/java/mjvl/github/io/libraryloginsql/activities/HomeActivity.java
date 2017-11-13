de1afea9b43ab311e051d51f77fb8f09293bae43
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-13 14:10:12 -0500

    Add GPS functionality

--- LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/HomeActivity.java
+++ LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/HomeActivity.java
@@ -7,6 +7,7 @@ package mjvl.github.io.libraryloginsql.activities;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import mjvl.github.io.libraryloginsql.R;
+import mjvl.github.io.libraryloginsql.helper.GPSTracker;
 import mjvl.github.io.libraryloginsql.helper.LibraryBounds;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -34,7 +36,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
         lblWelcome.setText("Logged in as " + nameFromIntent);
         btnCheck.setOnClickListener(this);
-        
     }
 
     @Override
@@ -42,6 +43,18 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.btnCheck:
                 double lat = 0, lon = 0;
+                GPSTracker GPS = new GPSTracker(HomeActivity.this);
+                // check if GPS enabled
+                if(GPS.canGetLocation()){
+                    lat = GPS.getLatitude();
+                    lon = GPS.getLongitude();
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + lat + "\nLong: " + lon, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    GPS.showSettingsAlert();
+                }
                 if (LibraryBounds.checkBounds(lat,lon)) {
                     Toast.makeText(getApplicationContext(), "In Bounds", Toast.LENGTH_SHORT).show();
                 }
