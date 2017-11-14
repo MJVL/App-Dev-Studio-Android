d25ae89854b2aa7c20fb5043c63e76a54b60f929
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-14 14:04:25 -0500

    Remove unused line

--- LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/RegisterActivity.java
+++ LibraryLoginSQL/app/src/main/java/mjvl/github/io/libraryloginsql/activities/RegisterActivity.java
@@ -41,7 +41,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-       //getSupportActionBar().hide();
 
         nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
         txtName = (EditText) findViewById(R.id.txtName);
