8c10c166af5269e0e7f6e40a04d05b729f5f2da8
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2017-11-30 14:07:42 -0500

    Fix bounds

--- Classes/LibraryBounds.java
+++ Classes/LibraryBounds.java
@@ -3,8 +3,8 @@ public class LibraryBounds {
     private final static double MAX_LAT = 39.35066;
     private final static double MIN_LAT = 39.35044;
 
-    private final static double MAX_LON = 84.30696;
-    private final static double MIN_LON = 84.30664;
+    private final static double MAX_LON = -84.30696;
+    private final static double MIN_LON = -84.30664;
     
     public static double getMaxLat() {
         return MAX_LAT;
@@ -23,7 +23,7 @@ public class LibraryBounds {
     }
 
     public static boolean checkBounds(double lat, double lon) {
-        if (lat <= MAX_LAT && lat >= MIN_LAT && lon <= MAX_LON && lon >= MIN_LON) {
+        if (lat <= MAX_LAT && lat >= MIN_LAT && lon => MAX_LON && lon <= MIN_LON) {
             return true;
         }
         return false;
